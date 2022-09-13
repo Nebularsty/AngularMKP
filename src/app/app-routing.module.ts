1a50f038de30cfb25ca67561797923d1d124306f
Nebularsty <nicolasoliveira9635@gmail.com>
2022-09-12 21:13:42 -0300

    remoção fundo filtragem e logo para voltar para home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { MonteOSeuComponent } from './pages/monte-o-seu/monte-o-seu.component';
 
 const routes: Routes = [
+  {path:'home', component: HomeComponent},
   {path:'monte-o-seu', component: MonteOSeuComponent},
   {path: '', component: HomeComponent}
 ];
