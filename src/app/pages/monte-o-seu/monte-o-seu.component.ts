865786f806239fec99fe9f43fa14336a6ae56033
unknown <kaua.szczepanski@etec.sp.gov.br>
2022-10-25 20:35:45 -0300

    Criando pag-pergunta

--- src/app/pages/monte-o-seu/monte-o-seu.component.ts
+++ src/app/pages/monte-o-seu/monte-o-seu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
 
 @Component({
   selector: 'app-monte-o-seu',
@@ -12,4 +13,49 @@ export class MonteOSeuComponent implements OnInit {
   ngOnInit(): void {
   }
 
+}
+
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  color: ThemePalette;
+  subtasks?: Task[];
+}
+
+/**
+ * @title Basic checkboxes
+ */
+export class CheckboxOverviewExample {
+  task: Task = {
+    name: 'Indeterminate',
+    completed: false,
+    color: 'primary',
+    subtasks: [
+      {name: 'Primary', completed: false, color: 'primary'},
+      {name: 'Accent', completed: false, color: 'accent'},
+      {name: 'Warn', completed: false, color: 'warn'},
+    ],
+  };
+
+  allComplete: boolean = false;
+
+  updateAllComplete() {
+    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
+  }
+
+  someComplete(): boolean {
+    if (this.task.subtasks == null) {
+      return false;
+    }
+    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
+  }
+
+  setAll(completed: boolean) {
+    this.allComplete = completed;
+    if (this.task.subtasks == null) {
+      return;
+    }
+    this.task.subtasks.forEach(t => (t.completed = completed));
+  }
 }
